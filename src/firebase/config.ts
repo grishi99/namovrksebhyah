b16ec7c592836478c8606f73b7afb70c2ddb711b
grishi99 <grishi99@gmail.com>
2025-12-18 18:13:59 +0530

    c

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-1349219632-73b3d",
-  "appId": "1:365749394278:web:0c64321eeb3bf4fb3606a7",
-  "apiKey": "AIzaSyDbMYIUUM4bbXl2JdKcIq80636tecbd-Rg",
-  "authDomain": "studio-1349219632-73b3d.firebaseapp.com",
-  "storageBucket": "studio-1349219632-73b3d.appspot.com",
+  "projectId": process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-1349219632-73b3d",
+  "appId": process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:365749394278:web:0c64321eeb3bf4fb3606a7",
+  "apiKey": process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDbMYIUUM4bbXl2JdKcIq80636tecbd-Rg",
+  "authDomain": process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-1349219632-73b3d.firebaseapp.com",
+  "storageBucket": process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET || "studio-1349219632-73b3d.appspot.com",
   "measurementId": "",
-  "messagingSenderId": "365749394278"
+  "messagingSenderId": process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "365749394278"
 };
